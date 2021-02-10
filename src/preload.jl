d6cee7542290626de2bee94f69c2b0b16678316c
Toby Driscoll <driscoll@udel.edu>
2021-02-10 14:46:06 -0500

    removed kNN for logistic

--- src/preload.jl
+++ src/preload.jl
@@ -3,15 +3,14 @@ using CSV
 using Clustering
 using DataFrames
 using HTTP
-using IJulia
 using MLJ
 using MLJClusteringInterface 
 using MLJMultivariateStatsInterface
 using MultivariateStats
-using NearestNeighborModels
 using Plots
 pyplot()
-using PyCall
 using Statistics 
 using StatsBase
 using UrlDownload
+using IJulia
+notebook()
