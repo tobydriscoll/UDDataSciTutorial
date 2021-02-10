1add6c2598690bc91ec86b32bd37a077295c94cf
Toby Driscoll <driscoll@udel.edu>
2021-02-10 09:30:11 -0500

    content added

---
+++ src/UDDataSciTutorial.jl
@@ -0,0 +1,5 @@
+module UDDataSciTutorial
+
+greet() = print("Hello World!")
+
+end # module
---
+++ src/preload.jl
@@ -0,0 +1,17 @@
+ENV["PYTHON"]=""
+using CSV
+using Clustering
+using DataFrames
+using HTTP
+using IJulia
+using MLJ
+using MLJClusteringInterface 
+using MLJMultivariateStatsInterface
+using MultivariateStats
+using NearestNeighborModels
+using Plots
+pyplot()
+using PyCall
+using Statistics 
+using StatsBase
+using UrlDownload
